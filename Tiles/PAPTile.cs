ea35baa383aa0fefa76500222b163a3acc3524c9
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 21:40:35 -0400

    Add files via upload

---
+++ Tiles/PAPTile.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+
+namespace AvariceExpansions.Tiles
+{
+    public class PAPTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.Origin = new Point16(1, 2);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Pack-a-Punch");
+            AddMapEntry(new Color(255, 255, 255), name);
+        }
+
+
+        public override void KillMultiTile( int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, Mod.Find<ModItem>("PAP").Type);
+        }
+    }
+}
