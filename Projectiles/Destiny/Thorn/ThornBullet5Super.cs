d7abdf9265373a4f70c8c124428aa346d894eedd
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 21:55:06 -0400

    Add files via upload

---
+++ Projectiles/Destiny/Thorn/ThornBullet5Super.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace AvariceExpansions.Projectiles.Destiny.Thorn
+{
+    public class ThornBullet5Super : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("ThornBullet5Super");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 6;
+            Projectile.height = 6;
+            Projectile.aiStyle = 1;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ranged = true;
+            Projectile.alpha = 0;
+            Projectile.light = .5f;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1;
+            AIType = ProjectileID.Bullet;
+            DrawOffsetX = -4;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(Mod.Find<ModBuff>("DevourMark5Super").Type, 300);
+        }
+        
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+    }
+}
