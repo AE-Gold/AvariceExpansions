02f6a5a120bab364cd128cc41e2127c09d29a625
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 21:42:46 -0400

    Add files via upload

---
+++ New/Ultimatum/UltimatumFireShot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace AvariceExpansions.Projectiles.New.Ultimatum
+{
+    public class UltimatumFireShot : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("UltimatumFireShot");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = 1;
+            Projectile.width = 2;
+            Projectile.height = 2;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ranged = true;
+            Projectile.alpha = 0;
+            Projectile.light = .5f;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1;
+            AIType = ProjectileID.Bullet;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (target.life <= 0)
+            {
+                Projectile.NewProjectile((Projectile.Center.X + Main.rand.Next(-250, 250)), Projectile.Center.Y - 500f, 0, 16f, Mod.Find<ModProjectile>("UltimatumFire").Type, 35, 1, Main.myPlayer);
+                Projectile.NewProjectile((Projectile.Center.X + Main.rand.Next(-250, 250)), Projectile.Center.Y - 500f, 0, 16f, Mod.Find<ModProjectile>("UltimatumFire").Type, 35, 1, Main.myPlayer);
+                Projectile.NewProjectile((Projectile.Center.X + Main.rand.Next(-250, 250)), Projectile.Center.Y - 500f, 0, 16f, Mod.Find<ModProjectile>("UltimatumFire").Type, 35, 1, Main.myPlayer);
+            }
+            target.AddBuff(153, 180); /* Buff 153 is Shadowflame */
+            target.AddBuff(204, 180); /* Buff 204 is Oiled */
+        }
+    }
+}
