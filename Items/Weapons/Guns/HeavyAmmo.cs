474e75048e1d2b517fba9b164c4f6a3dd41bd354
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 22:06:12 -0400

    Add files via upload

---
+++ Items/Weapons/Guns/HeavyAmmo.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AvariceExpansions.Items.Weapons.Guns.Destiny
+{
+	public class HeavyAmmo : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Heavy Ammo");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 25;
+			Item.ranged = true;
+			Item.width = 8;
+			Item.height = 8;
+			Item.maxStack = 999;
+			Item.consumable = true;
+			Item.knockBack = 1f;
+			Item.value = Item.sellPrice(0, 0, 1, 0);
+			Item.rare = 11;
+			Item.shoot = Mod.Find<ModProjectile>("KineticBullet").Type;
+			Item.shootSpeed = 16f;
+			Item.ammo = Mod.Find<ModItem>("HeavyAmmo").Type;
+		}
+	}
+}
