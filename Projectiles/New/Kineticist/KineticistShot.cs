2b5736597fe4ca8028174f7cba5062ce6ab5398e
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 21:53:05 -0400

    Add files via upload

---
+++ Projectiles/New/Kineticist/KineticistShot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace AvariceExpansions.Projectiles.New.Kineticist
+{
+    public class KineticistShot : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("KineticistShot");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = 1;
+            Projectile.width = 2;
+            Projectile.height = 2;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ranged = true;
+            Projectile.alpha = 0;
+            Projectile.light = .5f;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1;
+            AIType = ProjectileID.Bullet;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+    }
+}
