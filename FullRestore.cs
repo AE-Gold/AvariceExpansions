29bcc468d1e4340c48488e4ef32de3ec9c75104a
AE-Gold <53921477+AE-Gold@users.noreply.github.com>
2022-10-03 22:00:00 -0400

    Add files via upload

---
+++ FullRestore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AvariceExpansions.Items
+{
+    public class FullRestore : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Full Restore");
+            Tooltip.SetDefault("Dev Item");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 10;
+            Item.height = 10;
+            Item.rare = 10;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.useStyle = 4;
+            Item.UseSound = SoundID.Item4;
+            Item.healLife = 999;
+            Item.healMana = 999;
+            Item.consumable = false;
+        }
+    }
+}
