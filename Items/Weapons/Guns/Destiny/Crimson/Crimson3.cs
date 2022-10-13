4f88a6d8e411fd746d09a91da40812dcd620ba8a
ConnorMT <115058538+ConnorMT@users.noreply.github.com>
2022-10-13 09:51:33 -0400

    Update Crimson3.cs

--- Items/Weapons/Guns/Destiny/Crimson/Crimson3.cs
+++ Items/Weapons/Guns/Destiny/Crimson/Crimson3.cs
@@ -13,7 +13,7 @@ namespace AvariceExpansions.Items.Weapons.Guns.Destiny.Crimson
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Crimson");
-            Tooltip.SetDefault("Here's the remedy.\nMark 3");
+            Tooltip.SetDefault("Here's the remedy.\n[c/00A2C1:Leeches mana from enemies.]\n[c/02FF8A:Mark 3]");
         }
 
         public override void SetDefaults()
